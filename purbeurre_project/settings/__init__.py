6be6d26ed7de62586c479d976cc53b90bf8cd53c
nicoseng <nicolas.sengmany@gmail.com>
2022-11-23 03:00:54 +0100

    mise à jour .travis.yml

--- purbeurre_project/settings/__init__.py
+++ purbeurre_project/settings/__init__.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.1/ref/settings/
 
-
+#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
