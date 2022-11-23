d90b63b8e38bc7a4b5109fb73f426a8177ff6e6b
nicoseng <nicolas.sengmany@gmail.com>
2022-11-23 03:42:06 +0100

    test settings

--- purbeurre_project/settings/__init__.py
+++ purbeurre_project/settings/__init__.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'purbeurre_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'purbeurre_website/templates'), ],
+	'DIRS': [],
+        # 'DIRS': [os.path.join(BASE_DIR, 'purbeurre_website/templates'), ],
         # 'DIRS': ['purbeurre_website/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
